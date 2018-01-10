a82e17ebc706772bc178e480aab2414b175a31c6
krishna <krishna@krishna-PC>
2018-01-10 23:10:05 +0530

    test

---
+++ SpringAnalysis1/src/main/java/com/spflv/sa/SpringAnalysis1Application.java
@@ -0,0 +1,12 @@
+package com.spflv.sa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringAnalysis1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringAnalysis1Application.class, args);
+	}
+}
